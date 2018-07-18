c3b6c59dfe4faf2872b60110ef0cec0477753efa
swopet <swopet@oregonstate.edu>
2018-07-17 20:57:40 -0700

    add tile drawing

--- Tile.cpp
+++ Tile.cpp
@@ -2,11 +2,12 @@
 namespace Tile{
 	sf::Texture textures[2];
 	void load_textures(){
-		textures[0].loadFromFile("tex1.png");
-		textures[1].loadFromFile("tex2.png");
+		textures[0].loadFromFile("../Resources/Sprites/Tiles/checker_bw.png");
+		textures[1].loadFromFile("../Resources/Sprites/Tiles/checker_rb.png");
 	}
 	Tile::Tile()
 	{
+		texture = rand()%2;
 		height = 0.0;
 		pos = sf::Vector2f(0.0,0.0);
 	}
@@ -26,11 +27,19 @@ namespace Tile{
 	void Tile::draw(){
 		glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 		glColor3f(1.0,1.0,1.0);
+		glEnable(GL_TEXTURE_2D);
+		sf::Texture::bind(&textures[texture]);
 		glBegin(GL_QUADS);
+		glTexCoord2f(0.0,0.5);
 		glVertex3f(0.0,0.0,0.0);
+		glTexCoord2f(0.5,1.0);
 		glVertex3f(0.0,0.0,1.0);
+		glTexCoord2f(1.0,0.5);
 		glVertex3f(1.0,0.0,1.0);
+		glTexCoord2f(0.5,0.0);
 		glVertex3f(1.0,0.0,0.0);
 		glEnd();
+		sf::Texture::bind(NULL);
+		glDisable(GL_TEXTURE_2D);
 	}
 }
