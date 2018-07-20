2fc054ba7f8fdd9f58c738cf287776d64c032cec
William Selbie <selbiew@oregonstate.edu>
2018-07-19 20:38:04 -0700

    Added get/set to tile

--- Tile.cpp
+++ Tile.cpp
@@ -1,31 +1,53 @@
 #include "Tile.hpp"
-namespace Tile{
+namespace Tile {
 	sf::Texture textures[3];
-	void load_textures(){
+	
+	void load_textures() {
 		textures[BW].loadFromFile("../Resources/Sprites/Tiles/checker_bw.png");
 		textures[RB].loadFromFile("../Resources/Sprites/Tiles/checker_rb.png");
 		textures[GB].loadFromFile("../Resources/Sprites/Tiles/checker_gb.png");
 	}
-	Tile::Tile()
+	
+	Tile::Tile() 
 	{
 		type = GB;
 		height = 0.0;
 		pos = sf::Vector2f(0.0,0.0);
 	}
 
-	Tile::~Tile()
+	Tile::~Tile() 
 	{
 	}
-
-	void Tile::set_pos(sf::Vector2f new_pos){
-		pos = new_pos;
+	
+	GLfloat Tile::get_height() {
+		return height;
+	}
+	
+	void Tile::set_height(GLfloat height) {
+		this->height = height; 
+	}
+	
+	TileType Tile::get_type() { 
+		return type;
+	}
+	
+	void Tile::set_type(TileType type) {
+		this->type = type;
+	}
+	
+	sf::Vector2f Tile::get_pos() {
+		return pos;
 	}
 
-	void Tile::transform(){
+	void Tile::set_pos(sf::Vector2f pos) {
+		this->pos = pos;
+	}
+
+	void Tile::transform() {
 		glTranslatef(pos.x,height,pos.y);
 	}
 
-	void Tile::draw(){
+	void Tile::draw() {
 		glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 		glColor3f(1.0,1.0,1.0);
 		glEnable(GL_TEXTURE_2D);
--- Tile.hpp
+++ Tile.hpp
@@ -3,24 +3,35 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 namespace Tile{
-	enum tile_type {
+	enum TileType {
 		GB = 0,
 		RB = 1,
 		BW = 2
 	};
+	
 	void load_textures();
+	
 	class Tile
 	{
-	private:
-		tile_type type;
-		sf::Vector2f pos;
-		GLfloat height;
-	public:
-		Tile();
-		~Tile();
-		void set_pos(sf::Vector2f);
-		void transform();
-		void draw();
+		private:
+			TileType type;
+			sf::Vector2f pos;
+			GLfloat height;
+		public:
+			Tile();
+			~Tile();
+			
+			GLfloat get_height();
+			void set_height(GLfloat);
+			
+			TileType get_type();
+			void set_type(TileType);
+			
+			sf::Vector2f get_pos();
+			void set_pos(sf::Vector2f);
+			
+			void transform();
+			void draw();
 	};
 }
 #endif // TILE_HPP
